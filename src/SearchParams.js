8b0cf15cdfd36f84d72059cd82d67d502af8cc9f
schm00g <hk8igureu@relay.firefox.com>
2022-06-19 18:30:34 +0100

    breed list cache

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -19,7 +19,6 @@ const SearchParams = () => {
       `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
     );
     const json = await res.json();
-    console.log(`hello`, json.pets);
     setPets(json.pets);
   }
 
