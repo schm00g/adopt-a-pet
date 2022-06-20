f5ee96dae59fb9a189721746f7af0a3abb9108f2
schm00g <hk8igureu@relay.firefox.com>
2022-06-21 00:04:08 +0100

    class component adopt button

--- src/Details.js
+++ src/Details.js
@@ -1,8 +1,50 @@
+import { Component } from "react";
 import { useParams } from "react-router-dom";
 
-const Details = () => {
-  const { id } = useParams();
-  return <h1>hi lol {id}</h1>;
+// cannot use hooks with classes
+
+class Details extends Component {
+  constructor(props) {
+    super(props);
+    // <this> is the instance of Detail
+    this.state = { loading: true };
+  }
+
+  async componentDidMount() {
+    const res = await fetch(
+      `https://pets-v2.dev-apis.com/pets?id=${this.props.params.id}`
+    );
+
+    const json = await res.json();
+
+    this.setState({ loading: false, ...json.pets[0] });
+  }
+
+  render() {
+    if (this.state.loading) {
+      return <h2>loading...</h2>;
+    }
+
+    const { animal, breed, city, state, description, name } = this.state;
+
+    return (
+      <div className="details">
+        <div>
+          <h1>{name}</h1>
+          <h2>
+            {animal} - {breed} - {city}, {state}
+          </h2>
+          <button>Adopt {name}</button>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+const WrappedDetails = () => {
+  const params = useParams();
+  return <Details params={params} />;
 };
 
-export default Details;
+export default WrappedDetails;
