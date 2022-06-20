5474901c8b948ceebb40b8912be30e899e9e44b7
schm00g <hk8igureu@relay.firefox.com>
2022-06-20 14:29:20 +0100

    component abstraction

--- src/Pet.js
+++ src/Pet.js
@@ -1,12 +1,22 @@
 import React from "react";
 
-const Pet = ({ name, animal, breed }) => {
+const Pet = ({ name, animal, breed, images, location, id }) => {
+  let hero = "http://pet-images.dev-apis.com/pets/none.jpg";
+  if (images.length) {
+    hero = images[0];
+  }
   return (
-    <div>
-      <h1>{name}</h1>
-      <h1>{animal}</h1>
-      <h1>{breed}</h1>
-    </div>
+    <a href={`/details/${id}`} className="pet">
+      <div className="image-container">
+        <img src={hero} alt={name}></img>
+      </div>
+      <div className="info">
+        <h1>{name}</h1>
+        <h2>
+          {animal} — {breed} — {location}
+        </h2>
+      </div>
+    </a>
   );
 };
 
