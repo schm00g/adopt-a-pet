93760c87c85e07f89c50844af6a22806178ffb34
schm00g <hk8igureu@relay.firefox.com>
2022-06-20 19:45:25 +0100

    add react router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { StrictMode } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { render } from "react-dom";
 import SearchParams from "./SearchParams";
+import Details from "./Details";
 
 const App = () => {
   return (
-    <div>
-      <h1>Adopt Me!</h1>
-      <SearchParams />
-    </div>
+    <StrictMode>
+      <BrowserRouter>
+        <h1>Adopt Me!</h1>
+        <Routes>
+          <Route path="/details/:id" element={<Details />}></Route>
+          <Route path="/" element={<SearchParams />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </StrictMode>
   );
 };
 
---
+++ src/Details.js
@@ -0,0 +1,5 @@
+const Details = () => {
+  return <h1>hi lol</h1>;
+};
+
+export default Details;
