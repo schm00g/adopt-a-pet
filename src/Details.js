d2b2b285265d6835a594789a98309eddb60780e5
schm00g <hk8igureu@relay.firefox.com>
2022-06-20 21:12:49 +0100

    useParams

--- src/Details.js
+++ src/Details.js
@@ -1,5 +1,8 @@
+import { useParams } from "react-router-dom";
+
 const Details = () => {
-  return <h1>hi lol</h1>;
+  const { id } = useParams();
+  return <h1>hi lol {id}</h1>;
 };
 
 export default Details;
