5a5f407beec91cc5099ca37400ddb7b27a6ebca5
schm00g <hk8igureu@relay.firefox.com>
2022-06-22 16:12:25 +0100

    carousel interactivity

--- src/Details.js
+++ src/Details.js
@@ -1,5 +1,6 @@
 import { Component } from "react";
 import { useParams } from "react-router-dom";
+import Carousel from "./Carousel";
 
 // cannot use hooks with classes
 
@@ -21,10 +22,14 @@ class Details extends Component {
       return <h2>loading...</h2>;
     }
 
-    const { animal, breed, city, state, description, name } = this.state;
+    const { animal, breed, city, state, description, name, images } =
+      this.state;
+
+    console.log(images);
 
     return (
       <div className="details">
+        <Carousel images={images} />
         <div>
           <h1>{name}</h1>
           <h2>
